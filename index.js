da93798ded1dcf64b19492167d40c886539e960a
Joao Victor <joao.teixeira@globaldotcom.con.br>
2024-08-26 23:15:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({ hello: "world" });
+
+})
+
+app.listen(3000, () => {
+    console.log('Server started at localhost:3000');
+    
+})
